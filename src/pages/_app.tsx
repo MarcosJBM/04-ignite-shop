0af75fac803c779a22a31c77b02b9bce2e5413f9
Marcos Botene <marcosjbotene@gmail.com>
2022-09-11 18:02:23 -0300

    created home structure and styles.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { globalStyles } from '../styles/global';
 
 import logoImg from '../assets/logo.svg';
 import { Container, Header } from '../styles/pages/app';
-import Image from 'next/image';
+import Image from 'next/future/image';
 
 globalStyles();
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import Image from 'next/future/image';
+import { HomeContainer, Product } from '../styles/pages/home';
+
+import firstShirt from '../assets/shirts/1.png';
+import secondShirt from '../assets/shirts/2.png';
+import thirdShirt from '../assets/shirts/3.png';
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={firstShirt} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={secondShirt} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
