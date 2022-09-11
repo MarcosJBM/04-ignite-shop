2ec74a59a96e67261343d2cd29e7cc59f1ea5e29
Marcos Botene <marcosjbotene@gmail.com>
2022-09-11 16:41:13 -0300

    created application header.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from 'next/app';
 import { globalStyles } from '../styles/global';
 
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
+import Image from 'next/image';
+
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt='Ignite Shop' />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
