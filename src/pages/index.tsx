2e2b0f29f3ed014f3c9959746866f3411e27d4fe
Marcos Botene <marcosjbotene@gmail.com>
2022-09-11 18:18:22 -0300

    slide of products created.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Image from 'next/future/image';
+import { useKeenSlider } from 'keen-slider/react';
 import { HomeContainer, Product } from '../styles/pages/home';
 
 import firstShirt from '../assets/shirts/1.png';
 import secondShirt from '../assets/shirts/2.png';
 import thirdShirt from '../assets/shirts/3.png';
 
+import 'keen-slider/keen-slider.min.css';
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <Image src={firstShirt} width={520} height={480} alt='' />
 
         <footer>
@@ -17,8 +27,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className='keen-slider__slide'>
         <Image src={secondShirt} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={thirdShirt} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={thirdShirt} width={520} height={480} alt='' />
 
         <footer>
           <strong>Camiseta X</strong>
